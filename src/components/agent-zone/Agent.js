473b2825d596ee35d6cb87e53f8c3dff7a3830d2
root <rodrigojaviergd@gmail.com>
2022-06-01 17:01:44 +0000

    DESDE EL SERVER MELI

--- src/components/agent-zone/Agent.js
+++ src/components/agent-zone/Agent.js
@@ -1,6 +1,5 @@
 import React, {
     useState,
-    useEffect
 } from 'react'
 import HeaderAgent from '../agent-zone/HeaderAgent'
 import Loader from '../Loader'
@@ -12,7 +11,8 @@ const Agent = () => {
     const handleGrantedUsers = async () => {
         try {
             const response = await axios.get(USERS_GRANTED_LIST, { headers: { 'Content-Type': 'application/json' } });
-            setGrantedList(response?.['data'].filter((el) => el['status_'] === 'active').sort((a, b) => a['registration_date'] < b['registration_date']));
+            setGrantedList(response?.['data'].sort((a, b) => a['registration_date'] < b['registration_date']));
+            //setGrantedList(response?.['data'].filter((el) => el['status_'] === 'active').sort((a, b) => a['registration_date'] < b['registration_date']));
             setIsLoading(false);
         } catch (error) { console.log('ERROR GRANTEWd YUSES sELERS', error); }
     }
@@ -30,47 +30,49 @@ const Agent = () => {
             <Background />
             {!isLoading
                 ?
-                <>
-                    <div className="sellers-list-box">
-                        <h4 className='title_settings'> Total Granted Sellers ({grantedList.length})
-                            {/* ,  Inactives ({grantedList.filter((el) => el['status_'] === 'inactive').length}) */}
-                        </h4>
-                        <table className="table table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th> # </th>
-                                    <th> ID </th>
-                                    <th> NickName </th>
-                                    <th> Status </th>
-                                    <th> Registration Date </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {grantedList.map((el, index) => {
-                                    const i = index + 1;
-                                    let classic = '';
-                                    let textClass = '';
-                                    if (el['status_'] === 'active') { classic = 'moving'; textClass = 'active-danger'; }
-                                    if (el['status_'] === 'inactive') { classic = 'cancelled'; textClass = 'inactive-danger'; }
-                                    return (
-                                        <tr className='warning' key={`${index}`}>
-                                            <td> {i} </td>
-                                            <td> <b>{el['id']}</b> </td>
-                                            <td> <b>{el['nickname']}</b> </td>
-                                            <td>
-                                                <div className={`tm-status-circle ${classic}`}></div>
-                                                <em><b className={textClass}>{el['status_'].toUpperCase()}</b></em>
-                                            </td>
-                                            <td> <b>{el['registration_date'].split('T')[0]}</b> </td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </>
-                :
-                <Loader />
+                    <>
+                        <div className="sellers-list-box">
+                            <h4 className='title_settings'> 
+                                Total Granted Sellers ({grantedList.length})
+                                ,  Inactives ({grantedList.filter((el) => el['refresh_token'].length === 0)})
+                            </h4>
+                            <table className="table table-hover table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th> # </th>
+                                        <th> ID </th>
+                                        <th> NickName </th>
+                                        <th> Status </th>
+                                        <th> Registration Date </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {grantedList.map((el, index) => {
+                                        const i = index + 1;
+                                        let classic = '';
+                                        let textClass = '';
+                                        let textActive = '';
+                                        if (el['refresh_token'].length > 0) { classic = 'moving'; textClass = 'active-danger'; textActive = 'ACTIVE';}
+                                        else{  classic = 'cancelled'; textClass = 'inactive-danger'; textActive = 'INACTIVE'; }
+                                        return (
+                                            <tr className='warning' key={`${index}`}>
+                                                <td> {i} </td>
+                                                <td> <b>{el['id']}</b> </td>
+                                                <td> <b>{el['nickname']}</b> </td>
+                                                <td>
+                                                    <div className={`tm-status-circle ${classic}`}></div>
+                                                    <em><b className={textClass}>{textActive}</b></em>
+                                                </td>
+                                                <td> <b>{el['registration_date'].split('T')[0]}</b> </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+                    :
+                    <Loader />
 
 
             }
