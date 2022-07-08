d7d324f009579ce7e7ba8770a14a5759ad139524
Rodrigo Javier <rodrigojaviergd@gmail.com>
2022-07-08 16:35:40 -0400

    view build

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Loader = () => {
+
     return (
         <div className='body-loader'>
             <ul className="ul-loader">
-                <li className="li-loader" data-title="l">l</li>
+                <li className="li-loader" data-title="l">{'l'}</li>
                 <li className="li-loader" data-title="o">o</li>
                 <li className="li-loader" data-title="a">a</li>
                 <li className="li-loader" data-title="d">d</li>
