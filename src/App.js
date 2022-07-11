0f1dc6bc557b9e8ff8ba7ce951ab62004a8a7458
Rodrigo Javier <rodrigojaviergd@gmail.com>
2022-07-11 10:59:19 -0400

    view build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes, Route, useLocation } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Users from './components/users-zone/Users';
 import ReportsUser from './components/users-zone/ReportsUser';
 import ProductsUser from './components/users-zone/ProductsUser';
